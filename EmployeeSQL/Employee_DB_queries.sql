11845e4b871b3a4157f30317597a69bb46ff5acf
umiranda <71298635+umiranda@users.noreply.github.com>
2020-11-27 00:13:04 -0500

    Update Employee_DB_queries.sql

--- EmployeeSQL/Employee_DB_queries.sql
+++ EmployeeSQL/Employee_DB_queries.sql
@@ -65,9 +65,11 @@ ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no
 REFERENCES "departments" ("dept_no");
 
 -- List the following details of each employee: employee number, last name, first name, sex, and salary.
+
 SELECT  employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary 
 FROM employees 
 JOIN salaries on employees.emp_no = salaries.emp_no
+
 
 --List first name, last name, and hire date for employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
@@ -124,6 +126,9 @@ FROM employees
 GROUP BY last_name
 ORDER BY count(last_name) desc
 
-
+-- Epilogue
+SELECT employees.last_name, employees.first_name, employees.sex
+from employees 
+where emp_no = 499942
 
 
